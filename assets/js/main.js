cc5989e97f5246dfee72fd839c37d4b83dc11ba5
ak2002 <p.calanna@gmail.com>
2026-08-31 12:47:41 +0200

    Update

--- assets/js/main.js
+++
@@ -1,3 +0,0 @@
-// Registers the <pagefind-*> custom elements used by the search panel. All other
-// behaviour is Alpine, declared inline in the templates that own the markup.
-import '@pagefind/component-ui';
